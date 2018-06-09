b699c79042623517c4b0984aaeb713ab3617dace
Dominic Schaff <dominic.schaff@gmail.com>
2018-06-09 20:27:13 +0200

    Generate a sample file

--- app/src/main/java/zz/utility/QuoteActivity.kt
+++ app/src/main/java/zz/utility/QuoteActivity.kt
@@ -2,8 +2,13 @@ package zz.utility
 
 import android.app.Activity
 import android.os.Bundle
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.activity_quote.*
 import zz.utility.helpers.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 data class Quote(val quote: String, val author: String)
 
@@ -17,7 +22,34 @@ class QuoteActivity : Activity() {
         doAsync({
             val quotes = ArrayList<Quote>()
             tryIgnore {
-                val gson = "$HOME/quotes.json".fileAsJsonArray()
+
+                val f = File("$HOME/quotes.json")
+                if (!f.exists()) {
+                    val json = JsonArray()
+                    json.add(
+                            JsonObject()
+                                    .add("quote", "There are no quotes stored, update the file: $HOME/quotes.json")
+                                    .add("author", "This application")
+                    )
+                    var stream: FileOutputStream? = null
+                    try {
+                        stream = FileOutputStream(f, true)
+                        stream.write(json.toString().toByteArray())
+                        stream.flush()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        try {
+                            if (stream != null) stream.close()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+
+
+
+                val gson = f.asJsonArray()
                 gson.forEach {
                     val quote = it.asJsonObject
                     quotes.add(Quote(quote.s("quote"), quote.s("author")))
