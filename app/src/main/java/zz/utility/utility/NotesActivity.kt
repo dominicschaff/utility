b9a84dfcec6aecd5c4d3dad578b669d6c7ddd383
Dominic Schaff <dominic@schaff.dev>
2022-03-20 16:57:57 +0200

    Enable error ignore

--- app/src/main/java/zz/utility/utility/NotesActivity.kt
+++ app/src/main/java/zz/utility/utility/NotesActivity.kt
@@ -5,14 +5,17 @@ import android.os.Bundle
 import zz.utility.configFile
 import zz.utility.databinding.ActivityNotesBinding
 import zz.utility.externalFile
+import zz.utility.helpers.ignore
 
 class NotesActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNotesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNotesBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.text.setText(externalFile("utility.json").readText())
+        setContentView(binding.root);
+        {
+            binding.text.setText(externalFile("utility.json").readText())
+        }.ignore()
         binding.save.setOnClickListener {
             externalFile("utility.json").writeText(binding.text.text.toString())
         }
